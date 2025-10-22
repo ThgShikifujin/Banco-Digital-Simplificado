6f8f05c60e1516c256c3fc7f74159260b6a071e0
ThgShikifujin <thiagosilva_skt@outlook.com>
2025-10-22 19:00:47 -0300

    refactor: adicionando descrição da classe e Guard Clauses nas funções depositar e sacar

--- Conta.py
+++ Conta.py
@@ -1,5 +1,3 @@
-from Cliente import Cliente
-
 class Conta:
     # criando a lista de contas
     def __init__(self, numero, saldo, tipo):
@@ -7,14 +5,23 @@ class Conta:
         self.saldo: float = saldo
         self.tipo: str = tipo
 
+    def __str__(self):
+        return f"Conta: {self.numero}, saldo: {self.saldo}, tipo: {self.tipo}"
+
     # Depositar saldo na conta
     def depositar(self, valor):
+        if valor <= 0:
+            print(f"Falha ao depositar R${valor}, o valor não pode ser negativo ou igual a 0")
+            return
         self.saldo += valor
         print(f"Depósito de R${valor} efetuado com sucesso! Depósito: R${valor} Saldo Atual :R${self.saldo}")
 
     # Sacar saldo da conta verificando a conta e saldo disponivel
     def sacar(self, valor):
-        if valor <= conta.saldo:
+        if valor <= 0:
+            print(f"Falha ao sacar R${valor}, o valordo saque não pose ser negativo ou igual a 0")
+            return
+        if valor <= self.saldo:
             self.saldo -= valor
             print(f"Saque de R${valor} efetuado com sucesso!")
             print(f"Saldo atual: R${self.saldo}")
@@ -24,4 +31,4 @@ class Conta:
 
     # Extrato da conta, verificando conta
     def extrato(self):
-        print(f"Saldo Atual: R${conta.saldo}")
+        print(f"Saldo Atual: R${self.saldo}")
